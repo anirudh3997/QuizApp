a62a0d1c479ca4e9a4a8a507c40b7f03a134ed4d
anirudh3997 <anirudh.mangalvedhe@nineleaps.com>
2019-02-14 14:13:13 +0530

    Anirudh - Minor Changes

--- app/src/main/java/com/example/quiz/ShowName.java
+++ app/src/main/java/com/example/quiz/ShowName.java
@@ -1,7 +1,10 @@
 package com.example.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ShowName extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class ShowName extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_name);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        TextView tvParticipantName = findViewById(R.id.tvParticipantName);
+
+        tvParticipantName.setText(name);
     }
+
+
+    public void  StartQuiz(View view){
+        Intent intent = new Intent(ShowName.this, QwithImage.class);
+    }
+
 }
